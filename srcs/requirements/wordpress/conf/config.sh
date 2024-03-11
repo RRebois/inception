f6c16838e064e09c5cfd629e02529dd341922379
moi <rr@rr.com>
2024-03-11 17:25:29 +0100

    better than before

--- srcs/requirements/wordpress/conf/config.sh
+++ srcs/requirements/wordpress/conf/config.sh
@@ -2,8 +2,7 @@
 # sleep 10
 if [ ! -f $/var/www/html/wordpress/wp-config.php ]
 then
-    cd /var/www/html/wordpress/
-fi   
+    cd /var/www/html/wordpress/  
 
     wp core download --allow-root
 
@@ -13,7 +12,10 @@ fi
     --dbpass=$PASSWORD \
     --dbhost=mariadb:3306 --path='/var/www/html/wordpress'
 
-    wp core install --allow-root --url=$URL --title=$TITLE --admin=$ADMIN --admin_password=$ROOT_PASSWORD --admin_email=$ADMIN_MAIL
-    wp user create --allow-root test test@example.com --role=$CLIENT 
-    /usr/sbin/php-fpm7.4 -F
+    wp core install --allow-root --url=$URL --title=$TITLE --admin_user=$ADMIN --admin_password=$ROOT_PASSWORD --admin_email=$ADMIN_MAIL
+    wp user create --allow-root $USER_BIS $USER_BIS_MAIL
+    
+ fi
+    mkdir -p /run/php/
+    php-fpm7.4 -F
 
